d0edd03d31d41a015dcf7fcc68ed11554ad8d9c9
tomek-m-coder <60541005+tomek-m-coder@users.noreply.github.com>
2024-08-12 14:35:26 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,24 @@
+// Music
+const songs = [
+    {
+      name: 'Rising',
+      displayName: 'Adventure-Type-Beat',
+      artist: 'Thomas-Anstey-Lewis',
+    },
+    {
+     name: 'Freeway',
+      displayName: 'Adventure-Type-Beat',
+      artist: 'Thomas-Anstey-Lewis',
+    },
+    {
+     name: 'Forsaken',
+      displayName: 'Adventure-Type-Beat',
+      artist: 'Thomas-Anstey-Lewis',
+    },
+    {
+      name: 'Overcoming',
+      displayName: 'Adventure-Type-Beat',
+      artist: 'Thomas-Anstey-Lewis',
+    },
+  ];
+  
